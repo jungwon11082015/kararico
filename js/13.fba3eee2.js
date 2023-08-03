a6a441fe9b3d10887f6cd04fe19d71719459da86
jungwon11082015 <79095163+jungwon11082015@users.noreply.github.com>
2023-08-03 17:41:32 +0900

    Add files via upload

---
+++ js/13.fba3eee2.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkproject"]=self["webpackChunkproject"]||[]).push([[13],{7013:function(n,r,e){e.r(r),e.d(r,{default:function(){return p}});var t=e(3396);const u={id:"about"},c=(0,t._)("div",{class:"inner"},[(0,t._)("h2",null,"ABOUT"),(0,t._)("h3",null,"도전을 즐기는 삶, 실패를 두려움 없는 실패 너머의 추구 ")],-1),o=[c];function s(n,r,e,c,s,a){return(0,t.wg)(),(0,t.iD)("div",u,o)}var a={name:"About",props:{}},i=e(89);const l=(0,i.Z)(a,[["render",s]]);var p=l}}]);
+//# sourceMappingURL=13.fba3eee2.js.map
